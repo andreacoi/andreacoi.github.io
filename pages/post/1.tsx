4839628d6eb770878aa0683ff82fa98836a9f884
Andrea <andrea.coi@outlook.it>
2022-11-01 13:25:16 +0100

    test routing next

---
+++ pages/post/1.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+function PostOne() {
+  return <div>PostOne</div>
+}
+
+
+export default PostOne
