36192d315fe16bfb6f9848cf4382a2d606f9ef07
Andrea <andrea.coi@outlook.it>
2023-12-26 18:13:36 +0100

    funzione render markdown con dettagli articolo

---
+++ lib/articolo.tsx
@@ -0,0 +1,23 @@
+import { remark } from "remark";
+import html from "remark-html";
+import * as fs from "fs";
+import * as path from "path";
+import * as matter from "gray-matter";
+
+let path_articoli = "./data/articoli/";
+export async function singoloArticolo(id: number) {
+  const fullpath = path.resolve(path.join(path_articoli + "01-hello-world.md"));
+  const filecontent = fs.readFileSync(fullpath, "utf-8");
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(filecontent);
+
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+  let dati_articolo = [matterResult, processedContent];
+  let serial_dati = JSON.stringify(dati_articolo);
+  return serial_dati;
+}
+
+export async function ottieniArticoli() {}
