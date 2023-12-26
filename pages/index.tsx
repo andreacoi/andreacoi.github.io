730db111d9ccf01a518cc7aeb56c2b31c70f3ff7
Andrea <andrea.coi@outlook.it>
2023-12-26 20:49:26 +0100

    crea abbozzo di layout per articolo singolo in homepage
    da spostare in view articolo singolo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import styles from "../styles/Home.module.css";
 import { singoloArticolo } from "../lib/articolo";
 export default function Home({ postData }: any) {
+  const dati_articolo = JSON.parse(postData);
+  const titolo_articolo = dati_articolo[0].data.title;
+  const categoria = dati_articolo[0].data.categoria;
+  const data_pubblicazione = dati_articolo[0].data.pubblicazione;
+  const tags = dati_articolo[0].data.tags;
+  const contenuto = dati_articolo[1].value;
+  console.log(dati_articolo);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Tech Blog</h1>
+        <h2 className="titolo-articolo">{titolo_articolo}</h2>
+        <h3 className="categoria">{categoria}</h3>
+        <span className="data_pubblicazione">{data_pubblicazione}</span>
+        <span className="tags">{tags}</span>
+        <div dangerouslySetInnerHTML={{ __html: contenuto }} />
       </main>
-      <div dangerouslySetInnerHTML={{ __html: postData.content }} />
       <footer className={styles.footer}></footer>
     </div>
   );
