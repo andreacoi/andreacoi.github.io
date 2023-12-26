ad8bdd9852f5aa0e45223af0234b03cf364dcf54
Andrea <andrea.coi@outlook.it>
2023-12-26 18:16:54 +0100

    render articolo singolo su home - TEST

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,25 @@
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import clientPromise from "../lib/mongodb";
-
-export default function Home({ articoli }: any) {
+import { singoloArticolo } from "../lib/articolo";
+export default function Home({ postData }: any) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Tech Blog</h1>
-        <div className="lista-articoli">
-          {articoli.map((articolo: any) => (
-            <div className="articolo" key={articolo._id}>
-              <h2>{articolo.titolo}</h2>
-              <h3>{articolo.introtext}</h3>
-              <p>{articolo.fulltext}</p>
-            </div>
-          ))}
-        </div>
       </main>
-
+      <div dangerouslySetInnerHTML={{ __html: postData.content }} />
       <footer className={styles.footer}></footer>
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  try {
-    const client = await clientPromise;
-    const db = client.db("andreacoiblog");
-
-    const articoli = await db
-      .collection("articoli")
-      .find({})
-      .limit(1000)
-      .toArray();
-
-    return {
-      props: { articoli: JSON.parse(JSON.stringify(articoli)) },
-    };
-  } catch (e) {
-    console.error(e);
-  }
+export async function getStaticProps({ params }: any) {
+  // Add the "await" keyword like this:
+  const postData = await singoloArticolo(1);
+  // let list = Object.entries(postData).map([key, value]);
+  //console.log(list);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
