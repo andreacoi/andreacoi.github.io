313acb7ac46fcaa868dd33ee69fdcdec633390cd
Andrea <andrea.coi@outlook.it>
2023-12-29 19:11:10 +0100

    modifica file index per mostrare lista articoli

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,23 @@
-import styles from "../styles/Home.module.css";
-import { singoloArticolo } from "../lib/articolo";
+import { ottieniArticoli } from "../lib/articolo";
+import parse from "html-react-parser";
 export default function Home({ postData }: any) {
   const dati_articolo = JSON.parse(postData);
-  const titolo_articolo = dati_articolo[0].data.title;
-  const categoria = dati_articolo[0].data.categoria;
-  const data_pubblicazione = dati_articolo[0].data.pubblicazione;
-  const tags = dati_articolo[0].data.tags;
-  const contenuto = dati_articolo[1].value;
-  console.log(dati_articolo);
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Tech Blog</h1>
-        <h2 className="titolo-articolo">{titolo_articolo}</h2>
-        <h3 className="categoria">{categoria}</h3>
-        <span className="data_pubblicazione">{data_pubblicazione}</span>
-        <span className="tags">{tags}</span>
-        <div dangerouslySetInnerHTML={{ __html: contenuto }} />
-      </main>
-      <footer className={styles.footer}></footer>
+  return dati_articolo.map((articolo: Array<any>) => (
+    <div key="{articolo[0].data.id}" className="post">
+      <h2 className="titolo-articolo">{articolo[0].data.title}</h2>
+      <h3 className="categoria">{articolo[0].data.categoria}</h3>
+      <span className="data_pubblicazione">
+        {articolo[0].data.pubblicazione}
+      </span>
+      <span className="tags">{articolo[0].data.tags}</span>
+      <div className="content">{parse(articolo[1].value)}</div>
     </div>
-  );
+  ));
 }
 
 export async function getStaticProps({ params }: any) {
   // Add the "await" keyword like this:
-  const postData = await singoloArticolo(1);
+  const postData = await ottieniArticoli();
   // let list = Object.entries(postData).map([key, value]);
   //console.log(list);
   return {
