1a8b0710a0ef5ba96b2a5caa3ec6d415d82675b7
Andrea <andrea.coi@outlook.it>
2023-12-31 12:20:00 +0100

    provo a generare staticamente un path senza raccogliere dati

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -56,7 +56,8 @@ export default function Single({ postData }: any) {
 
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
   return {
-    paths: [], //indicates that no page needs be created at build time
+    //const paths = posts.map((post) => `/post/${slug}`),
+    paths: ["post/01-nuovo-blog"], //indicates that no page needs be created at build time
     fallback: "blocking", //indicates the type of fallback
   };
 };
