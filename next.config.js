7281f27de95cb6b93ee39c193732f42db472b570
Andrea <andrea.coi@outlook.it>
2023-12-31 11:59:48 +0100

    aggiunge configurazione url

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
-
-module.exports = nextConfig
+};
+(module.exports = nextConfig),
+  {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId },
+    ) {
+      return {
+        "/": { page: "/" },
+        "/about": { page: "/about" },
+        "/post/01-nuovo-blog": { page: "/post/01-nuovo-blog" },
+      };
+    },
+  };
