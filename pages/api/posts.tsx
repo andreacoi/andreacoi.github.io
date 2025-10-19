edd772b00cab344ee1ca498580714450331a01cd
Andrea Coi <andrea.coi@outlook.it>
2025-10-19 11:19:43 +0200

    reset sistema di blog --> passaggio a jekyll

--- pages/api/posts.tsx
+++
@@ -1,8 +0,0 @@
-import { ottieniArticoli } from "../../lib/articolo";
-
-export default async (req: any, res: any) => {
-  try {
-    let lista_articoli = ottieniArticoli();
-    res.json(lista_articoli);
-  } catch {}
-};
